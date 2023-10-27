03fc04620ad4ec92a83e645b1da7efc40f41b102
SantioMC <business@santio.me>
2023-10-26 21:51:57 -0400

    fix: Add fields to description targets

--- bukkit/src/main/java/me/santio/coffee/bukkit/annotations/Description.kt
+++ bukkit/src/main/java/me/santio/coffee/bukkit/annotations/Description.kt
@@ -4,7 +4,7 @@ package me.santio.coffee.bukkit.annotations
  * Represents a description for a command.
  */
 @Retention(AnnotationRetention.RUNTIME)
-@Target(AnnotationTarget.CLASS, AnnotationTarget.FUNCTION)
+@Target(AnnotationTarget.CLASS, AnnotationTarget.FUNCTION, AnnotationTarget.FIELD)
 annotation class Description(
     val value: String
 )
