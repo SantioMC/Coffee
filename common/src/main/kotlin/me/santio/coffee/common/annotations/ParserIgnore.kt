a17dec48e64e2e120a0333b899f2bb232cebef90
SantioMC <business@santio.me>
2023-10-26 20:31:42 -0400

    fix: add proper targets for ParserIgnore

--- common/src/main/kotlin/me/santio/coffee/common/annotations/ParserIgnore.kt
+++ common/src/main/kotlin/me/santio/coffee/common/annotations/ParserIgnore.kt
@@ -4,5 +4,5 @@ package me.santio.coffee.common.annotations
  * Indicates that the annotated property should be ignored by the parser.
  */
 @Retention(AnnotationRetention.RUNTIME)
-@Target(AnnotationTarget.CLASS, AnnotationTarget.FUNCTION)
+@Target(AnnotationTarget.CLASS, AnnotationTarget.FUNCTION, AnnotationTarget.VALUE_PARAMETER, AnnotationTarget.PROPERTY, AnnotationTarget.FIELD)
 annotation class ParserIgnore
